dc1617e38302f49e18e5efaf5df5a05028b1ba15
Winston Shields <winstonjshields@gmail.com>
2020-11-29 00:59:54 -0500

    Changed board to 8x8

--- state.js
+++ state.js
@@ -1,6 +1,6 @@
 class State {
     constructor() {
-        this.board = Array(6).fill().map(() => Array(6).fill(null));
+        this.board = Array(8).fill().map(() => Array(8).fill(null));
         this.blackStoneLives = 0;
         this.whiteStoneLives = 0;
         this.terminal = false;
